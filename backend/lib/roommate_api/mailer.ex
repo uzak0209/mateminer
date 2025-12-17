a32b3139cabe5e4965fba9f5a593695847259dfa
uzak0209 <116805788+uzak0209@users.noreply.github.com>
2025-12-17 15:43:51 +0900

    remove elixir files

--- backend/lib/roommate_api/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule RoommateApi.Mailer do
-  use Swoosh.Mailer, otp_app: :roommate_api
-end
--- backend/lib/roommate_api/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule RoommateApi.Repo do
-  use Ecto.Repo,
-    otp_app: :roommate_api,
-    adapter: Ecto.Adapters.Postgres
-end
